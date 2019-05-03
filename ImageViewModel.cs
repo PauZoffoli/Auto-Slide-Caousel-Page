93e5d15bb335dfee832a3c81f87411826606b25f
Paulina Zoffoli <paulina0296@hotmail.es>
2019-05-03 14:22:06 -0400

    Auto Slider CarouselPage
    Auto Slider CarouselPage

---
+++ ImageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CarouselPageNavigation
+{
+    public class ImageViewModel : NotificationObject
+    {
+
+        public static ObservableCollection<ImageViewModel> All { get; set; }
+
+
+        private string imageSource;
+
+        public string ImageSource
+
+        {
+
+            get { return imageSource; }
+
+            set { imageSource = value; OnPropertyChanged(); }
+
+        }
+        private Color colores;
+
+        public Color Colores
+
+        {
+
+            get { return colores; }
+
+            set { colores = value; OnPropertyChanged(); }
+
+        }
+
+
+        static ImageViewModel()
+        {
+            All = new ObservableCollection<ImageViewModel> {
+                new ImageViewModel {
+                    ImageSource = "http://192.168.20.38:8000/api/imagenes/divinagabriela/banner/banner1",
+                    Colores = Color.Red
+                },
+                new ImageViewModel {
+                    ImageSource = "http://192.168.20.38:8000/api/imagenes/divinagabriela/banner/banner2",
+                    Colores = Color.Green
+                },
+                new ImageViewModel {
+                    ImageSource = "http://192.168.20.38:8000/api/imagenes/divinagabriela/banner/banner3",
+                    Colores = Color.Blue
+                }
+            };
+        }
+
+
+    
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace CarouselPageNavigation
+{
+	public partial class MainPage : CarouselPage
+	{
+
+       
+        public int SlidePosition { get; set; }
+        public MainPage ()
+		{
+			InitializeComponent ();
+
+
+            ItemsSource = ImageViewModel.All;
+
+            AutoSlide();
+
+        }
+
+       
+
+        public void AutoSlide()
+        {
+            int currentPage = 0;
+            Device.StartTimer(TimeSpan.FromSeconds(3), () =>
+            {
+                this.CurrentPage = this.Children[currentPage];
+
+                if (currentPage == ImageViewModel.All.Count-1)
+                {
+                    currentPage = 0;
+                }
+                else
+                {
+                    currentPage++;
+                }
+
+                Console.WriteLine("HI");
+              
+                return true;
+               
+            });
+
+
+        }
+
+
+    }
+}
+
